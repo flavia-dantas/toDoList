94072af46658dde025fc7a026f16b0077fcb33f9
flavia-dantas <flavia.dantas2@hotmail.com>
2024-01-26 23:05:55 -0300

    fix: adjustment footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex h-[50px] items-center justify-center bg-gray-700 text-xs text-gray-300">
+    <footer className="flex h-[50px] items-center justify-center bg-gray-700 text-xs text-gray-300 fixed bottom-0 w-full">
       <div>
         <p>
           &copy; Copyright 2024 | Developed by{" "}
