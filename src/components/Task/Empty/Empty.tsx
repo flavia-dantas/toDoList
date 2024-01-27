94dfedb88acd1d05b6cac2d2cb3f7686597bfd32
flavia-dantas <flavia.dantas2@hotmail.com>
2024-01-26 21:42:59 -0300

    feat: task empty component

---
+++ src/components/Task/Empty/Empty.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import IconClipboard from "../../../assets/clipboard.svg";
+
+const Empty = () => (
+  <div className="flex flex-col items-center justify-center gap-4 rounded-lg border-t-[1px] border-solid border-t-gray-400 px-6 py-16">
+    <Image src={IconClipboard} alt="icon clipboard" />
+
+    <div>
+      <p className="text-base font-bold text-gray-300">
+        Você ainda não tem tarefas cadastradas
+      </p>
+
+      <p className="text-base text-gray-300">
+        Crie tarefas e organize seus itens a fazer
+      </p>
+    </div>
+  </div>
+);
+
+export default Empty;
