f536016c6fb25cb118344c161c12c9488e8ea494
flavia-dantas <flavia.dantas2@hotmail.com>
2024-01-26 21:42:32 -0300

    feat: task component

---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { Empty } from "./Empty";
+import { HeaderTask } from "./HeaderTask";
+import { ItemTask } from "./ItemTask";
+import { TasksContext } from "@/contexts";
+
+const Task = () => {
+  const { tasks } = useContext(TasksContext);
+  return (
+    <div className="w-[736px]">
+      <HeaderTask />
+      {tasks?.length > 0 ? <ItemTask /> : <Empty />}
+    </div>
+  );
+};
+
+export default Task;
